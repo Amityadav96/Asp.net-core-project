227ddcbb44d70037764f75970b023fe748e585f1
Amityadav96 <76735023+Amityadav96@users.noreply.github.com>
2023-08-21 18:16:19 +0530

    Controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleProjects.DAL.Infrastructure;
+using SimpleProjects.Data;
+using SimpleProjects.Models;
+
+namespace SimpleProjects.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private IUnitofWork _unit;
+
+        public CategoryController(IUnitofWork unit)
+        {
+            _unit = unit;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _unit.CategoryRepository.GetAll();
+            return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _unit.CategoryRepository.Add(category);
+                _unit.Save();
+                return RedirectToAction("Index");
+
+            }
+
+            return View(category);
+        }
+    }
+}
