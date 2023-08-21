da18f933a022ae160f9ff27c8d787e062a224356
Amityadav96 <76735023+Amityadav96@users.noreply.github.com>
2023-08-21 18:13:28 +0530

    Model class

---
+++ SimpleProjects.Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleProjects.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public string ? Name { get; set; }
+        public int Display { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
---
+++ SimpleProjects.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleProjects.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
